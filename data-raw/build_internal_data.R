8f83991270bb7b1ce749f996714638e9ce60c90b
Tanja Proctor <proctor@imbi.uni-heidelberg.de>
2021-12-02 14:46:58 +0100

    add data with covariates, our example

--- data-raw/build_internal_data.R
+++ data-raw/build_internal_data.R
@@ -13,3 +13,22 @@ tsd2ex5bugsnet <- read.csv(paste0(getwd(), "/data-raw/TSD2_ex5_parkinsons_bugsne
                            stringsAsFactors = FALSE, check.names = F)
 tsd2ex5 <- list(data = tsd2ex5data, results = tsd2ex5results, bugsnet = tsd2ex5bugsnet)
 usethis::use_data(tsd2ex5, internal = TRUE)
+
+
+# Add data for extension enrichment-through weighting, how to do it internal?
+library(readxl)
+data_bsp <- Daten_Beispiel <- read_excel(paste0(getwd(),"/Daten_Beispiel.xlsx"))
+data1 <- data_bsp %>%
+  select(study, treament, control,response_treat, response_cont, n_treat, n_cont, cov) %>%
+  rename(
+    treatment_1 = "treament",
+    treatment_2 = "control",
+    event_1 = "response_treat",
+    event_2 = "response_cont",
+    n_1 = "n_treat",
+    n_2 = "n_cont",
+    x = "cov"
+  ) %>%
+  pivot_longer(c(2:7),
+               names_to = c(".value", "set"),
+               names_pattern = "(.+)_(.)")
